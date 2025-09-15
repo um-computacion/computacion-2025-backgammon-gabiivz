6d753d5c6c89c8c66c01314627b31fd54b2a1f72
gabiivz <g.vaca@alumno.um.edu.ar>
2025-09-15 16:43:18 -0300

    Clase checker

--- core/checker.py
+++ core/checker.py
@@ -1,3 +1,19 @@
 class Checker:
-    pass
+    def __init__(self, ficha, color, posicion, jugador):
+        self.__checker__ = ficha
+        self.__color__ = color
+        self.__jugador__ = jugador
+        self.__posicion__ = posicion
+    
+    def get_ficha(self):
+        return self.__checker__
+
+    def get_color_y_jugador(self):
+        return (self.__color__, self.__jugador__)
+    
+    def get_posicion(self):
+        return self.__posicion__
+
+
+        
 #ficha
