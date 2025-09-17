6f633ea2c87f57f266525e90794c9c6abf52fd18
gabiivz <g.vaca@alumno.um.edu.ar>
2025-09-17 16:45:27 -0300

    Clase backgammongame

--- tests/tests_checker.py
+++ tests/tests_checker.py
@@ -4,15 +4,12 @@ from core.checker import Checker
 class TestChecker(unittest.TestCase):
 
     def test_get_ficha(self):
-        checker = Checker("Ficha1", "Blanco", 5, "Juan")
+        checker = Checker("Ficha1", "Blanco", "Juan")
         self.assertEqual(checker.get_ficha(), "Ficha1")
     
-    def test_get_posicion(self):
-        checker = Checker("Ficha1", "Blanco", 10, "Juan")
-        self.assertEqual(checker.get_posicion(), 10)
     
     def test_jugador_y_color(self):
-        checker = Checker("Ficha1", "Blanco", 5, "Juan")
+        checker = Checker("Ficha1", "Blanco", "Juan")
         self.assertEqual(checker.get_color_y_jugador(), ("Blanco", "Juan"))
     
 if __name__ == '__main__':
